b90de62c26cab79fe5baa08e5a955874b1f9d24c
Pascal Lantam Gbate <cyrillantam@gmail.com>
2026-05-25 14:17:41 +0000

    Fix rate litmit for render proxy

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
